ee250ffa0b70d02fb3bf92f3ef86a1e193bb01fe
mgjeffries <gib.jeffries@gmail.com>
2020-09-03 12:15:16 -0400

    add styling

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { LocationPovider } from "./locations/LocationProvider"
 import { LocationList } from "./locations/LocationList"
+import "./KandyKorner.css"
+
 
 export const KandyKorner = () => (
   <>
-    <div>Kandy Korner</div>
+    <div className="kandy-korner">
+      <div>Kandy Korner</div>
 
-    <h2>Locations</h2>
-    <LocationPovider>
-      <LocationList />
-    </LocationPovider>
+      <h2>Locations</h2>
+      <LocationPovider>
+        <LocationList />
+      </LocationPovider>
+    </div>
   </>
 )

fb74862aa45ac5a7d15e14cebe9182f024eff017
mgjeffries <gib.jeffries@gmail.com>
2020-09-03 12:10:33 -0400

    add location components

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { LocationPovider } from "./locations/LocationProvider"
+import { LocationList } from "./locations/LocationList"
 
 export const KandyKorner = () => (
-  <div>Kandy Korner</div>
+  <>
+    <div>Kandy Korner</div>
+
+    <h2>Locations</h2>
+    <LocationPovider>
+      <LocationList />
+    </LocationPovider>
+  </>
 )

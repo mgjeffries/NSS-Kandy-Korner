1fd8996318b401dd72ef02ee537d9e69143edca5
mgjeffries <gib.jeffries@gmail.com>
2020-09-10 17:06:17 -0500

    show whether employyes are managers and fulltime

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -6,8 +6,8 @@ export const Employee = ({employee}) => {
     <div className="employee" >
       <div className="employee__name">Name: {employee.name}</div>
       <div className="employee__location">Location: {employee.location.address}</div>
-      <div className="employee__isManager">Manager: {employee.isManager}</div>
-      <div className="employee__isFullTime">Full Time:{employee.isFullTime}</div>
+      <div className="employee__isManager">Manager: {employee.isManager ? "Yes" : "No"}</div>
+      <div className="employee__isFullTime">Full Time: {employee.isFullTime ? "Yes" : "No"}</div>
       <div className="employee__hourlyRate">Hourly Rate: {employee.hourlyRate}</div>
     </div>
   )
